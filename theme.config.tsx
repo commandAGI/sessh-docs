0540a864ea7ca1cf94a9e12f636a5fec05c7a719
Jacob Valdez <jacobfv123@gmail.com>
2025-11-05 08:12:12 -0800

    Initial docs site with Nextra

---
+++ theme.config.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const config = {
+  logo: <span>Sessh Documentation</span>,
+  project: {
+    link: 'https://github.com/CommandAGI/sessh',
+  },
+  docsRepositoryBase: 'https://github.com/CommandAGI/sessh-docs',
+  footer: {
+    text: 'Sessh Documentation © 2025',
+  },
+  primaryHue: 220,
+  primarySaturation: 100,
+}
+
+export default config
